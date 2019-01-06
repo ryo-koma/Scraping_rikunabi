99a5845b615b0132679ec1e0542359eb7ad8e068
yuki-kondo <yuki.kondo@dena.com>
2019-01-06 18:34:07 +0900

    fix ruby format

--- list_rikunabi_1.rb
+++ list_rikunabi_1.rb
@@ -2,12 +2,13 @@ require 'open-uri'
 require 'nokogiri'
 require 'csv'
 require 'byebug'
+require 'pry'
 
 def get_data(uri, companies)
   html = open(uri).read
   documents = Nokogiri::HTML(html)
   #byebug
-  documents.xpath("//a[@class='ts-h-search-cassetteTitleMain js-h-search-cassetteTitleMain']").each {|n| companies << n.text.strip}
+  documents.xpath("//a[@class='ts-h-search-cassetteTitleMain js-h-search-cassetteTitleMain']").each { |n| companies << n.text.strip }
   return companies
 end
 
@@ -17,17 +18,17 @@ def main()
   page = gets.to_i
 
   companies = []
-  data = get_data(uri,companies)
+  data = get_data(uri, companies)
   (2..page).to_a.each do |idx|
     uri = "https://job.rikunabi.com/2019/s/__13_0_______/?moduleCd=2&isc=ps054&pn=#{idx}"
-    data = get_data(uri,companies)
+    data = get_data(uri, companies)
   end
 
   len = [companies.size].min - 1
 
   headers = ["会社名"]
   time = Time.new.strftime("%Y-%m-%d")
-  CSV.open("rikunabi_tokyo_2019-#{time}.csv", "a",headers: headers, write_headers: true) do |csv|
+  CSV.open("rikunabi_tokyo_2019-#{time}.csv", "a", headers: headers, write_headers: true) do |csv|
     (0..len).to_a.each do |idx|
       csv_column_values = [companies[idx]]
       csv << csv_column_values
